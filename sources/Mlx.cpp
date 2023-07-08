5464de7b0cd7e1b4234db91874810c63d1392d4c
etachott <bfwaend@gmail.com>
2023-07-08 20:20:04 -0300

    feat: add closing window clicking on X

--- sources/Mlx.cpp
+++ sources/Mlx.cpp
@@ -1,5 +1,6 @@
 #include "Mlx.hpp"
 
+/* Default builders ======================================================== */
 Mlx::Mlx() {
 	char windowName[] = "fdf";
 	this->_ptr = mlx_init();
@@ -12,7 +13,9 @@ Mlx::~Mlx() {
 	free(this->_ptr);
 	return ;
 }
+/* ========================================================================== */
 
+/* Window looping handling ================================================== */
 static inline
 int handleKeyPressEvents(int keyCode, Mlx &mlx) {
 	if (keyCode == 65307)
@@ -20,14 +23,23 @@ int handleKeyPressEvents(int keyCode, Mlx &mlx) {
 	return (0);
 }
 
+int clickPressEvents(Mlx &mlx) {
+	mlx.quit();
+	return (0);
+}
+
 void Mlx::loop(void) {
 	int (*hkpe)() = reinterpret_cast<int (*)()>(handleKeyPressEvents);
+	int (*cpe)() = reinterpret_cast<int (*)()>(clickPressEvents);
 
 	mlx_key_hook(this->_window, hkpe, this);
+	mlx_hook(this->_window, 17, 0, cpe, this);
 	mlx_loop(this->_ptr);
 	return ;
 }
+/* ========================================================================== */
 
+/* Window closing =========================================================== */
 void Mlx::quit(void) {
 	mlx_destroy_window(this->_ptr, this->_window);
 	mlx_destroy_display(this->_ptr);
@@ -35,3 +47,4 @@ void Mlx::quit(void) {
 		free(this->_ptr);
 	std::exit(0);
 }
+/* ========================================================================== */
